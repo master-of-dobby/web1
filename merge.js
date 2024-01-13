7d2e920d16b901512cd031c2c70a4963b6e8600e
master-of-dobby <naveenoffcial777@gmail.com>
2024-01-13 16:45:35 +0530

    sd

--- merge.js
+++ merge.js
@@ -1,9 +1,3 @@
 console.log("From another branch - new_branch1");
 console.log("hello from new_branch1");
 console.log("From user 2");
-console.log("hello 1");
-console.log("in nb1");
-console.log("hellooooooooooooooo nb111");
-console.log("----------------");
-console.log("/////////---end---/////////");
-console.log("byyyyyyyyye");
