739981d07c6396b3d93d8bead1cdd4e4ff05d875
master-of-dobby <naveenoffcial777@gmail.com>
2024-01-13 16:27:13 +0530

    main branch adding

--- merge.js
+++ merge.js
@@ -1,5 +1,5 @@
 console.log("From another branch - new_branch1");
 console.log("hello from new_branch1");
 console.log("From user 2");
-console.log("from main branch");
 console.log("hello 1");
+console.log("from main branch");
